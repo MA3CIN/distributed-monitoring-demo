d6330ebace2f12243153b55e8b09cc7eb5918eaf
MA3CIN <marcin.zet@onet.pl>
2023-09-13 10:25:38 +0200

    Working prometheus helm install

--- Open5GS_metrics/setup.sh
+++ Open5GS_metrics/setup.sh
@@ -3,8 +3,15 @@ helm repo add openverso https://gradiant.github.io/openverso-charts/
 helm install openverso/open5gs
 
 # Prometheus operator
-kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
-kubectl apply -f Prometheus/rbac_prometheus.yaml
-kubectl apply -f Prometheus/deploy.yaml
-kubectl apply -f Prometheus/svc_prometheus.yaml
-kubectl apply -f Prometheus/service_monitor_prometheus.yaml
+# kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
+# kubectl apply -f Prometheus/rbac_prometheus.yaml
+# kubectl apply -f Prometheus/deploy.yaml
+# kubectl apply -f Prometheus/svc_prometheus.yaml
+# kubectl apply -f Prometheus/service_monitor_prometheus.yaml
+
+# Prometheus operator, the Helm way
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install -f helm-prometheus-scrape-config.yaml prometheus prometheus-community/prometheus
+
+# Uninstall chart
+# helm uninstall prometheus
