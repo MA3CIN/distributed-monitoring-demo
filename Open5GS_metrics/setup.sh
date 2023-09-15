ff18518668799477bc985ace054acd92a82c8b70
MA3CIN <marcin.zet@onet.pl>
2023-09-15 14:13:45 +0200

    reorder helm installation order

--- Open5GS_metrics/setup.sh
+++ Open5GS_metrics/setup.sh
@@ -1,18 +1,3 @@
-# Helm Open5GS
-helm repo add openverso https://gradiant.github.io/openverso-charts/
-helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs
-
-
-# debug install
-# helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs --debug --dry-run
-
-# Prometheus operator
-# kubectl create -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
-# kubectl apply -f Prometheus/rbac_prometheus.yaml
-# kubectl apply -f Prometheus/deploy.yaml
-# kubectl apply -f Prometheus/svc_prometheus.yaml
-# kubectl apply -f Prometheus/service_monitor_prometheus.yaml
-
 # Prometheus operator, the Helm way
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/kube-prometheus-stack 
@@ -20,6 +5,15 @@ helm install prometheus prometheus-community/kube-prometheus-stack
 
 # Port forward prometheus svc
 kubectl port-forward svc/prometheus-operated 9090:9090
+
+# Helm Open5GS
+helm repo add openverso https://gradiant.github.io/openverso-charts/
+helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs
+
+# debug install
+# helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs --debug --dry-run > output.txt
+
+
 
 # Uninstall chart
 # helm uninstall prometheus
