39e75fcb404991fcd58d38b7b3ccb6e7dd5d2f78
MA3CIN <marcin.zet@onet.pl>
2023-10-17 10:57:33 +0200

    create cluster in setup, add teardown

--- Open5GS_metrics/setup.sh
+++ Open5GS_metrics/setup.sh
@@ -1,3 +1,6 @@
+#Create kind cluster
+kind create cluster
+
 # Prometheus operator, the Helm way
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/kube-prometheus-stack 
---
+++ Open5GS_metrics/teardown.sh
@@ -0,0 +1,3 @@
+helm uninstall prometheus
+helm uninstall open5gs
+kind delete cluster
