5742606df0bb0a05ec203db34ee81cc6f6dbe071
MA3CIN <marcin.zet@onet.pl>
2023-10-17 11:11:21 +0200

    get Grafana secrets

--- Open5GS_metrics/setup.sh
+++ Open5GS_metrics/setup.sh
@@ -7,16 +7,15 @@ helm install prometheus prometheus-community/kube-prometheus-stack
 # helm install -f helm-prometheus-scrape-config.yaml prometheus prometheus-community/prometheus
 
 # Port forward prometheus svc
+#kubectl wait pod do prometheusa 
 kubectl port-forward svc/prometheus-operated 9090:9090
 
 # Helm Open5GS
 helm repo add openverso https://gradiant.github.io/openverso-charts/
 helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs
 
+# Display localhost Grafana user and pass
+kubectl get secret prometheus-grafana -o jsonpath='{.data}'
+
 # debug install
 # helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs --debug --dry-run > output.txt
-
-
-
-# Uninstall chart
-# helm uninstall prometheus
