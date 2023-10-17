312d858abb480c3281000f61f0de1c958aba8b02
MA3CIN <marcin.zet@onet.pl>
2023-10-17 12:13:35 +0200

    secrets, forwarding + open url

--- Open5GS_metrics/setup.sh
+++ Open5GS_metrics/setup.sh
@@ -6,16 +6,22 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm install prometheus prometheus-community/kube-prometheus-stack 
 # helm install -f helm-prometheus-scrape-config.yaml prometheus prometheus-community/prometheus
 
-# Port forward prometheus svc
-#kubectl wait pod do prometheusa 
-kubectl port-forward svc/prometheus-operated 9090:9090
-
 # Helm Open5GS
 helm repo add openverso https://gradiant.github.io/openverso-charts/
 helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs
 
 # Display localhost Grafana user and pass
-kubectl get secret prometheus-grafana -o jsonpath='{.data}'
+kubectl get secret prometheus-grafana -o jsonpath='{.data.admin-user}' | base64 --decode
+kubectl get secret prometheus-grafana -o jsonpath='{.data.admin-password}' | base64 --decode
+
+# Port forward prometheus & grafana svc
+# kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=prometheus-kube-prometheus-prometheus --timeout=60s  
+# kubectl port-forward svc/prometheus-operated 9090:9090
+
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=grafana --timeout=60s  
+kubectl port-forward svc/prometheus-grafana 8080:80
+
+xdg-open http://localhost:8080
 
 # debug install
 # helm install --values open5gs-enable-metrics.yaml open5gs openverso/open5gs --debug --dry-run > output.txt
