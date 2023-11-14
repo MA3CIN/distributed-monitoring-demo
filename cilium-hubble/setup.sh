ca6ac98142db5ed1da9803acc781e7a932e0308c
MA3CIN <marcin.zet@onet.pl>
2023-11-14 11:39:46 +0100

    add teardown script, better progress visualization in setup script

--- cilium-hubble/setup.sh
+++ cilium-hubble/setup.sh
@@ -1,6 +1,9 @@
 ./scripts/setup-minikube.sh
+echo "----- Minikube setup finished -----"
 ./scripts/install-cilium.sh
+echo "----- Cilium setup finished -----"
 ./scripts/install-hubble.sh
+echo "----- Hubble setup finished -----"
 
 hubble status
 
